6d971bd0d1ac94c6055ecdd7b3e3b8c043c260c6
Kiyoshi Woolheater <Kiyoshi@kiyoshis-mbp.hsd1.ga.comcast.net>
2017-02-21 23:43:05 -0500

    collection view controller created and linked

---
+++ pickImage/CollectionViewController.swift
@@ -0,0 +1,60 @@
+//
+//  CollectionViewController.swift
+//  MemeMe 2.0
+//
+//  Created by Kiyoshi Woolheater on 2/21/17.
+//  Copyright © 2017 Kiyoshi Woolheater. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let space:CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+        
+        guard flowLayout != nil else {
+            return
+        }
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+    }
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    var memeData: [Meme] {
+        return appDelegate.memes
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.memeData.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
+        
+        let memeForRow = self.memeData[indexPath.row]
+        cell.memeImage?.image = memeForRow.memedImage
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "ShowMemeViewController") as! ShowMemeViewController
+        
+        //populate the data
+        let memeForRow = self.memeData[indexPath.row]
+        detailController.meme = memeForRow
+        
+        //push controller
+        navigationController!.pushViewController(detailController, animated: true)
+    }
+    
+}
---
+++ pickImage/MemeCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe 2.0
+//
+//  Created by Kiyoshi Woolheater on 2/21/17.
+//  Copyright © 2017 Kiyoshi Woolheater. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImage: UIImageView!
+    
+}
